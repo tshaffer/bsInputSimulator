297f6e505bab3eafd8864e2ed3b444f0360eb53f
Ted Shaffer <ted@brightsign.biz>
2017-12-05 19:36:59 -0800

    additional bp, udp development.

--- index.ts
+++ index.ts
@@ -1,14 +1,5 @@
 console.log("Hello world!");
 
-let currentCommand : string = '';
-
-// bp parameters
-// b -> bp900A
-// bb -> bp900B
-// ba -> bp200A
-// bab -> bp200B
-// etc.
-
 let buttonPanelType : string = '';
 let buttonPanelIndex : number = 0;
 let buttonNumber : number = 0;
@@ -21,6 +12,8 @@ let message : string = '';
 document.addEventListener('keydown', (event) => {
 
   const keyName = event.key;
+
+  console.log('keyName: ', keyName);
 
   if (keyName === 'Control' || keyName === 'Shift') {
     return;
@@ -50,95 +43,110 @@ document.addEventListener('keydown', (event) => {
 }, false);
 
 function parseInput(message : string) : any {
+
+  console.log('parse string: ', message);
+
   switch (message.charAt(0)) {
+    case 'b': {
+      return parseBpEvent(message);
+    }
+    case 'u': {
+      return parseUdpEvent(message);
+    }
     default: {
       return {
         command: 'unknown'
       };
     }
-    case 'b': {
-      // buttonPanel event
-      if (message.length > 1) {
-        switch (message.charAt(1)) {
-          default: {
-            // bp900a
-            buttonPanelType = 'bp900';
-            buttonPanelIndex = 0;
-            buttonNumber = getButtonNumber(message, 1);
+  }
+}
 
-            return {
-              command: 'bp',
-              parameters: {
-                buttonPanelType,
-                buttonPanelIndex,
-                buttonNumber
-              }
-            };
-          }
-          case 'b': {
-            // bp900B
-            buttonPanelType = 'bp900';
-            buttonPanelIndex = 1;
+function parseBpEvent(message : string) : any {
 
-            buttonNumber = -1;
-            if (message.length > 2) {
-              buttonNumber = getButtonNumber(message, 2);
+// bp parameters
+// b -> bp900A
+// bb -> bp900B
+// ba -> bp200A
+// bab -> bp200B
+// etc.
+
+  if (message.length > 1) {
+    switch (message.charAt(1)) {
+      default: {
+        return buildBpEvent('bp900', 0, getButtonNumber(message, 1));
+      }
+      case 'b': {
+        // bp900B
+        buttonNumber = message.length > 2 ? getButtonNumber(message, 2) : -1;
+        return buildBpEvent('bp900', 1, buttonNumber);
+      }
+      case 'c': {
+        // bp900C
+        buttonNumber = message.length > 2 ? getButtonNumber(message, 2) : -1;
+        return buildBpEvent('bp900', 2, buttonNumber);
+      }
+      case 'd': {
+        // bp900D
+        buttonNumber = message.length > 2 ? getButtonNumber(message, 2) : -1;
+        return buildBpEvent('bp900', 3, buttonNumber);
+      }
+      case 'a': {
+        // bp200
+        buttonPanelType = 'bp200';
+        if (message.length > 2) {
+          switch (message.charAt(2)) {
+            default: {
+              return buildBpEvent('bp200', 0, getButtonNumber(message, 2));
             }
-            return {
-              command: 'bp',
-              parameters: {
-                buttonPanelType,
-                buttonPanelIndex,
-                buttonNumber
-              }
-            };
-          }
-          case 'c': {
-            // bp900C
-            buttonPanelType = 'bp900';
-            buttonPanelIndex = 2;
-            buttonNumber = -1;
-            if (message.length > 2) {
-              buttonNumber = getButtonNumber(message, 2);
+            case 'b': {
+              buttonNumber = message.length > 3 ? getButtonNumber(message, 3) : -1;
+              return buildBpEvent('bp200', 1, buttonNumber);
             }
-            return {
-              command: 'bp',
-              parameters: {
-                buttonPanelType,
-                buttonPanelIndex,
-                buttonNumber
-              }
-            };
-          }
-          case 'd': {
-            // bp900D
-            buttonPanelType = 'bp900';
-            buttonPanelIndex = 3;
-            buttonNumber = -1;
-            if (message.length > 2) {
-              buttonNumber = getButtonNumber(message, 2);
+            case 'c': {
+              buttonNumber = message.length > 3 ? getButtonNumber(message, 3) : -1;
+              return buildBpEvent('bp200', 2, buttonNumber);
             }
-            return {
-              command: 'bp',
-              parameters: {
-                buttonPanelType,
-                buttonPanelIndex,
-                buttonNumber
-              }
-            };
-          }
-          case 'a': {
-            // bp200
-            buttonPanelType = 'bp200';
-            break;
+            case 'd': {
+              buttonNumber = message.length > 3 ? getButtonNumber(message, 3) : -1;
+              return buildBpEvent('bp200', 3, buttonNumber);         
+            }
           }
         }
+        break;
       }
     }
   }
+}
+
+function buildBpEvent(buttonPanelType: string, buttonPanelIndex: number, buttonNumber: number) : any {
+  return {
+    command: 'bp',
+    parameters: {
+      buttonPanelType,
+      buttonPanelIndex,
+      buttonNumber
+    }
+  };
 }
 
 function getButtonNumber(str : string, charIndex : number) : number {
   const charAtIndex = str.charAt(charIndex);
   return Number(charAtIndex);
 }
+
+function parseUdpEvent(message : string) : any {
+  if (message.length > 1) {
+    return {
+      command: 'udp',
+      parameters: {
+        command: message.substr(1)
+      }
+    }
+  }
+  else {
+    return {
+      command: 'unknown'
+    };
+  }
+}
+
